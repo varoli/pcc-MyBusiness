a35463f1ad9d0f5c57c58b8ab9a68ae91368e587
Javier Buron Gutierrez <abrew_javier@hotmail.com>
2015-08-04 11:40:10 -0500

    manejador bd a la mitad

--- src/procesos/ManejadorBd.java
+++ src/procesos/ManejadorBd.java
@@ -2,62 +2,156 @@ package procesos;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class ManejadorBd {
 	private Dato datoXml;
 	private Connection con;
-	/*String connectionUrl = "jdbc:sqlserver://WINDOWS-PC\\SQLEXPRESS:1394;" +
-			"databaseName=C:\\MyBusinessDatabase\\MyBusinessPOS2012.mdf;integratedSecurity=true;";*/
-	private String dbURL = "jdbc:sqlserver://localhost;databaseName=prueba";
-	private String user = "sa";
-	private String pass = "d12345678";
 	
 	//Javier Burón Gutiérrez (javier_buron_gtz@outlook.com)
 	//Lizeth Vásquez Rojas (liz_02277@hotmail.com)
 	
-	public static void main(String[] args){
-		//new ManejadorBd();
-		
-		System.out.println("Antes de conexión");
+	/**
+	 * Crea una conexión a la base de datos
+	 */
+	private void conectarBd(){
 		try {
 			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			/*String connectionUrl = "jdbc:sqlserver://127.0.0.1\\SQLEXPRESS;" +
-			"databaseName=prueba;user=;password=;integratedSecurity=true";*/
 			String connectionUrl = "jdbc:sqlserver://127.0.0.1\\SQLEXPRESS;" +
-					"databaseName=prueba;integratedSecurity=true";
-			Connection con = DriverManager.getConnection(connectionUrl);
-			if(con != null){
-				System.out.println("exito");
-			}
+					"databaseName=C:\\MyBusinessDatabase\\MyBusinessPOS2012.mdf;integratedSecurity=true";
+			con = DriverManager.getConnection(connectionUrl);
 		} catch (Exception e) {
-			System.out.println("error: "+e.toString());
+			System.out.println("Error: " + e.getMessage());
+			System.exit(-1);
 		}
-		System.out.println("fin");
 	}
 	
-	public ManejadorBd() {
+	/**
+	 * Cierra la conexión a la base de datos
+	 */
+	private void cerrarConexion(){
 		try {
-			Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-			con = DriverManager.getConnection(dbURL, user, pass);
-			//con = DriverManager.getConnection(connectionUrl);
-			System.out.println("conexion exitosa");
-		} catch (SQLException | ClassNotFoundException e) {
+			con.close();
+		} catch (SQLException e) {
 			System.out.println(e.getMessage());
 		}
 	}
 	
-	//Javier Burón Gutiérrez (javier_buron_gtz@outlook.com)
-	//Lizeth Vásquez Rojas (liz_02277@hotmail.com)
-	
+	/**
+	 * Este es el método principal que llamarán para guardar los datos XML dentro de la BD
+	 * @param datoXml contenido xml, este contenido es el que se guardará a la BD
+	 */
 	public void actualizarBd(Dato datoXml) {
 		this.datoXml = datoXml;
+		int ultimaColumnaAtributos = datoXml.getArticulos()[0].length - 1;
+		for(int i=0; i<datoXml.getArticulos().length; i++)
+			for(int j=0; j<datoXml.getArticulos().length; j++)
+				if(Boolean.parseBoolean(datoXml.getArticulos()[i][ultimaColumnaAtributos].toString()))
+					procesarArticulo(datoXml.getArticulos()[i]);
 	}
 	
-	private boolean actualizarTablaProds(){
+	/**
+	 * Realiza una acción en la BD.
+	 * @param sql La cadena con la consulta a realizar en la base de datos.
+	 * @return Respuesta de la BD, este resultado es retornado de tipo ResultSet.
+	 */
+	private ResultSet consultarBD(String sql){
+		try {
+			return con.createStatement().executeQuery(sql);
+			/*while (rs.next()){  
+               System.out.println(rs.getString(2));  
+            }*/
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}  
+		return null; 
+	}
+	
+	/**
+	 * Procesa un articulo para ser guardado en la BD
+	 * @param rowArticulo Contenido del articulo
+	 */
+	private void procesarArticulo(Object[] rowArticulo){
+		conectarBd();
+		String sqlExistenciaAlmacen = "";
+		String sqlProds = "";
+		String articulo = "N'" + rowArticulo[0].toString() + "'";
+		String descripcion = "N'" + rowArticulo[1].toString() + "'";
+		String unidad = "N'" + rowArticulo[2].toString() + "'";
+		String impuesto = "N'IVA'"; //IVA
+		float cantidad = Integer.parseInt(rowArticulo[3].toString());
+		float precio = Float.parseFloat(rowArticulo[4].toString()); //PRECIO1
+		float importe = Float.parseFloat(rowArticulo[5].toString()); //COSTO_U
+		int almacen = 9; //ALMACEN en tabla existenciaalmacen
+		boolean existenciaAlmacen = investigarArticulo(rowArticulo[0].toString(), "existenciaalmacen"); //rowArticulo[0].toString() --> articulo
+		boolean prods = investigarArticulo(rowArticulo[0].toString(), "prods"); //rowArticulo[0].toString() --> articulo
+		if(existenciaAlmacen && prods){
+			sqlExistenciaAlmacen = "";
+			sqlProds = "";
+		} else if(existenciaAlmacen && !prods){
+			sqlExistenciaAlmacen = "";
+			sqlProds = "";
+		} else if(!existenciaAlmacen && prods){
+			sqlExistenciaAlmacen = "";
+			sqlProds = "";
+		} else {
+			sqlExistenciaAlmacen = "INSERT INTO existenciaalmacen VALUES(" + almacen + ", " + articulo + ", " + cantidad + ")";
+			sqlProds = "INSERT INTO prods(ARTICULO, DESCRIP, PRECIO1, COSTO_U, UNIDAD, IMPUESTO) VALUES(" 
+					+ articulo + ", " + descripcion + ", " + precio + ", " + importe + ", " + unidad + impuesto + ")";
+		}
+		consultarBD(sqlExistenciaAlmacen);
+		consultarBD(sqlProds);
+		
+		actualizarExistenciaProd(articulo); //buscar una mejor posición para esta linea de código
+		cerrarConexion();
+	}
+	
+	private boolean investigarArticulo(String articulo, String tabla){
+		try {
+			ResultSet rs = consultarBD("SELECT count(articulo) FROM " + tabla + " WHERE articulo=N'" + articulo + "'"); //pienso falta en wehere un and almacen
+			while (rs.next()){  
+				if(Integer.parseInt(rs.getString(1)) > 0)
+					return true;
+					//actualizarDatos(1, rowArticulo); //PIENSO DEBERIA HABER VALIDACION TAMBIEN PARA SI EXISTE EN PRODS Y NO EN
+			}
+		} catch (SQLException e) {
+			System.out.println(e.getMessage());
+		}
 		return false;
 	}
-	private boolean actualizarTablaExistenciasAlmacen(){
-		return false;
+
+	/**
+	 * Actualiza los articulos en las tablas correspondientes de la BD.
+	 * @param almacen Número de almacen donde actualizar los articulos optenidos.
+	 * @param rowArticulo Lista de articulos a actualizar.
+	 */
+	private void actualizarDatos(int almacen, Object[] rowArticulo) {
+		String articulo = "N'" + rowArticulo[0].toString() + "'";
+		String descripcion = rowArticulo[1].toString();
+		String unidad = rowArticulo[2].toString();
+		float cantidad = Float.parseFloat(rowArticulo[3].toString()); //VER no me convence creo debe ser integer
+		float precio = Float.parseFloat(rowArticulo[4].toString()); //PRECIO1
+		float importe = Float.parseFloat(rowArticulo[5].toString()); //COSTO_U
+		
+		//Tabla prods
+		String sqlProds = "UPDATE prods SET DESCRIP= " + descripcion + ", PRECIO1=" + precio + ", COSTO_U=" + importe + ", UNIDAD=" + unidad + "WHERE ARTICULO=" + articulo;
+		consultarBD(sqlProds);
+		
+		//Tabla existenciaalmacen
+		String sqlExistenciaAlmacen = "UPDATE existenciaalmacen SET existencia = (SELECT existencia FROM existenciaalmacen WHERE (ALMACEN=" + almacen + ") AND (articulo=N'" + articulo + "')) + " + cantidad
+				+ "WHERE (almacen=1) AND (articulo=" + articulo + ")";
+		consultarBD(sqlExistenciaAlmacen);
+	}
+	
+	//Actualizar el campo existencia de la tabla prods
+	/**
+	 * Actualiza el campo existencia de un articulo en la tabla "prods"
+	 * @param articulo Nombre del articulo que será actualizada su existencia en la tabla "prods"
+	 */
+	private void actualizarExistenciaProd(String articulo){
+		String sql = "UPDATE prods SET prods.EXISTENCIA= (SELECT SUM(existenciaalmacen.existencia) FROM existenciaalmacen WHERE articulo= N'" + articulo + "') WHERE prods.ARTICULO= N'" + articulo + "'";
+		consultarBD(sql);
 	}
 }
